3b9e69359790255b0053ccce37be54cfe801c014
Art1523 <thoratatharva442@gmail.com>
2025-08-03 19:26:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,5 +40,7 @@ Last updated: {last_updated}
 
     return render_template("index.html", qr_generated=False)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
